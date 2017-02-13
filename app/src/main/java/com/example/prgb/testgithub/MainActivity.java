32f60181f49d5deeb3892e2b4a9d7ed18a1126ae
khanhduynd95 <khanhduynd95@gmail.com>
2017-02-13 16:45:35 +0700

    thay doi 1

--- app/src/main/java/com/example/prgb/testgithub/MainActivity.java
+++ app/src/main/java/com/example/prgb/testgithub/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        //hffdghgfdhh
     }
 }
